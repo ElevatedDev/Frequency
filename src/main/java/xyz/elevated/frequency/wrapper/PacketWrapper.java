6e7085945938954d845dada6332297d6a6a62f72
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-07-05 23:43:08 +0300

    Changed it to a weak hashmap to make sure there are no memory leaks

--- src/main/java/xyz/elevated/frequency/wrapper/PacketWrapper.java
+++ src/main/java/xyz/elevated/frequency/wrapper/PacketWrapper.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Field;
 import java.util.*;
 
 public abstract class PacketWrapper {
-    private final Map<String, Field> fields = Maps.newConcurrentMap();
+    private final Map<String, Field> fields = new WeakHashMap<>();
 
     private final Packet<?> instance;
     private final Class<? extends Packet<?>> clazz;
