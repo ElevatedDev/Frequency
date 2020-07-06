73b0e2085ab9dfb11addbd27be4718df169ad2ec
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-07-06 21:14:07 +0300

    Commited aura D

---
+++ src/main/java/xyz/elevated/frequency/check/impl/killaura/KillAuraD.java
@@ -0,0 +1,36 @@
+package xyz.elevated.frequency.check.impl.killaura;
+
+import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity;
+import xyz.elevated.frequency.check.CheckData;
+import xyz.elevated.frequency.check.type.PacketCheck;
+import xyz.elevated.frequency.data.PlayerData;
+import xyz.elevated.frequency.wrapper.impl.client.WrappedPlayInUseEntity;
+
+@CheckData(name = "KillAura (D)")
+public final class KillAuraD extends PacketCheck {
+    private int streak = 0;
+
+    public KillAuraD(final PlayerData playerData) {
+        super(playerData);
+    }
+
+    @Override
+    public void process(final Object object) {
+        if (object instanceof WrappedPlayInUseEntity) {
+            final WrappedPlayInUseEntity wrapper = (WrappedPlayInUseEntity) object;
+
+            if (wrapper.getAction() == PacketPlayInUseEntity.EnumEntityUseAction.ATTACK) {
+                final boolean invalid = !playerData.getActionManager().getSwinging().get();
+
+                // Player swung and attacked
+                if (invalid) {
+                    if (++streak > 2) {
+                        fail();
+                    }
+                } else {
+                    streak = 0;
+                }
+            }
+        }
+    }
+}
