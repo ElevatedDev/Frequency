bf412f1c93c91106733158f9f96483f32e44c8cc
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-07-06 21:20:40 +0300

    Commited KillAura E

---
+++ src/main/java/xyz/elevated/frequency/check/impl/killaura/KillAuraE.java
@@ -0,0 +1,49 @@
+package xyz.elevated.frequency.check.impl.killaura;
+
+import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity;
+import xyz.elevated.frequency.check.CheckData;
+import xyz.elevated.frequency.check.type.PacketCheck;
+import xyz.elevated.frequency.data.PlayerData;
+import xyz.elevated.frequency.wrapper.impl.client.WrappedPlayInFlying;
+import xyz.elevated.frequency.wrapper.impl.client.WrappedPlayInUseEntity;
+
+@CheckData(name = "KillAura (E)")
+public final class KillAuraE extends PacketCheck {
+    private int movements, lastMovements, total, invalid;
+
+    public KillAuraE(final PlayerData playerData) {
+        super(playerData);
+    }
+
+    @Override
+    public void process(final Object object) {
+        if (object instanceof WrappedPlayInUseEntity) {
+            final WrappedPlayInUseEntity wrapper = (WrappedPlayInUseEntity) object;
+
+            if (wrapper.getAction() == PacketPlayInUseEntity.EnumEntityUseAction.ATTACK) {
+                final boolean proper = movements < 4 && lastMovements < 4;
+
+                if (proper) {
+                    final boolean flag = movements == lastMovements;
+
+                    if (flag) {
+                        ++invalid;
+                    }
+
+                    if (++total == 25) {
+
+                        if (invalid > 20)
+                            fail();
+
+                        total = 0;
+                    }
+                }
+
+                lastMovements = movements;
+                movements = 0;
+            }
+        } else if (object instanceof WrappedPlayInFlying) {
+            ++movements;
+        }
+    }
+}
