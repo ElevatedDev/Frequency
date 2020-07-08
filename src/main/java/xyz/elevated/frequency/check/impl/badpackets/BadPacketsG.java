fd0848d80e622d79cf4bf3853d22b25747a50216
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-07-08 21:26:37 +0300

    Fixed

--- src/main/java/xyz/elevated/frequency/check/impl/badpackets/BadPacketsG.java
+++ src/main/java/xyz/elevated/frequency/check/impl/badpackets/BadPacketsG.java
@@ -1,5 +1,6 @@
 package xyz.elevated.frequency.check.impl.badpackets;
 
+import net.minecraft.server.v1_8_R3.PacketPlayInEntityAction;
 import xyz.elevated.frequency.check.CheckData;
 import xyz.elevated.frequency.check.type.PacketCheck;
 import xyz.elevated.frequency.data.PlayerData;
@@ -9,6 +10,7 @@ import xyz.elevated.frequency.wrapper.impl.client.WrappedPlayInFlying;
 @CheckData(name = "BadPackets (G)")
 public final class BadPacketsG extends PacketCheck {
     private int count = 0;
+    private PacketPlayInEntityAction.EnumPlayerAction lastAction;
 
     public BadPacketsG(final PlayerData playerData) {
         super(playerData);
@@ -17,11 +19,15 @@ public final class BadPacketsG extends PacketCheck {
     @Override
     public void process(final Object object) {
         if (object instanceof WrappedPlayInEntityAction) {
-            final boolean invalid = ++count > 1;
+            final WrappedPlayInEntityAction wrapper = (WrappedPlayInEntityAction) object;
+
+            final boolean invalid = ++count > 1 && wrapper.getAction() == lastAction;
 
             if (invalid) {
                 fail();
             }
+
+            this.lastAction = wrapper.getAction();
         } else if (object instanceof WrappedPlayInFlying) {
             count = 0;
         }
