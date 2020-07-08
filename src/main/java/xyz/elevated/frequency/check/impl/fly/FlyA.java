4afb57190479444fe4fe09fd5ee279c7f2b1e3aa
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-07-08 13:54:14 +0300

    Fixed buffer

---
+++ src/main/java/xyz/elevated/frequency/check/impl/fly/FlyA.java
@@ -0,0 +1,42 @@
+package xyz.elevated.frequency.check.impl.fly;
+
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import org.bukkit.Location;
+import xyz.elevated.frequency.check.CheckData;
+import xyz.elevated.frequency.check.type.PositionCheck;
+import xyz.elevated.frequency.data.PlayerData;
+import xyz.elevated.frequency.update.PositionUpdate;
+import xyz.elevated.frequency.util.NmsUtil;
+
+@CheckData(name = "Fly (A)")
+public final class FlyA extends PositionCheck {
+    private double buffer = 0.0;
+
+    public FlyA(final PlayerData playerData) {
+        super(playerData);
+    }
+
+    @Override
+    public void process(final PositionUpdate positionUpdate) {
+        // Get the locations from the position update
+        final Location from = positionUpdate.getFrom();
+        final Location to = positionUpdate.getTo();
+
+        // Get the entity player from the nms util
+        final EntityPlayer entityPlayer = NmsUtil.getEntityPlayer(playerData);
+
+        // If the posY of the player is modulo by (1/64) he's on ground.
+        final boolean clientGround = entityPlayer.onGround;
+        final boolean serverGround = to.getY() % 0.015625 == 0.0;
+
+        final boolean illegal = playerData.getPositionManager().getTouchingClimbable().get() || playerData.getPositionManager().getTouchingLiquid().get();
+
+        if (!illegal && clientGround != serverGround) {
+            if (++buffer > 4) {
+                fail();
+            }
+        } else {
+            buffer = 0;
+        }
+    }
+}
--- src/main/java/xyz/elevated/frequency/data/impl/PositionManager.java
+++ src/main/java/xyz/elevated/frequency/data/impl/PositionManager.java
@@ -1,22 +1,38 @@
 package xyz.elevated.frequency.data.impl;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.material.Stairs;
+import org.bukkit.material.Step;
 import xyz.elevated.frequency.check.type.PositionCheck;
+import xyz.elevated.frequency.data.BoundingBox;
 import xyz.elevated.frequency.data.PlayerData;
 import xyz.elevated.frequency.exempt.ExemptManager;
 import xyz.elevated.frequency.exempt.type.ExemptType;
+import xyz.elevated.frequency.observable.Observable;
 import xyz.elevated.frequency.update.PositionUpdate;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor @Getter
 public final class PositionManager {
+    @Getter(AccessLevel.NONE)
     private final PlayerData playerData;
 
+    @Getter(AccessLevel.NONE)
     private double lastPosX, lastPosY, lastPosZ;
 
+    private final Observable<Boolean> touchingAir = new Observable<>(false);
+    private final Observable<Boolean> touchingLiquid = new Observable<>(false);
+    private final Observable<Boolean> touchingHalfBlock = new Observable<>(false);
+    private final Observable<Boolean> touchingClimbable = new Observable<>(false);
+
     public void handle(final double posX, final double posY, final double posZ) {
+        this.handleCollisions(playerData);
+
         final World world = playerData.getBukkitPlayer().getWorld();
         final Player bukkitPlayer = playerData.getBukkitPlayer();
 
@@ -49,5 +65,22 @@ public final class PositionManager {
         this.lastPosX = posX;
         this.lastPosY = posY;
         this.lastPosZ = posZ;
+    }
+
+    private void handleCollisions(final PlayerData playerData) {
+        final World world = playerData.getBukkitPlayer().getWorld();
+        final BoundingBox boundingBox = playerData.getBoundingBox().get();
+
+        boundingBox.expand(0.075, 0.075, 0.075).move(0.0, -0.55, 0.0);
+
+        final boolean touchingAir = boundingBox.checkBlocks(world, material -> material == Material.AIR);
+        final boolean touchingLiquid = boundingBox.checkBlocks(world, material -> material == Material.WATER || material == Material.LAVA || material == Material.STATIONARY_WATER || material == Material.STATIONARY_LAVA);
+        final boolean touchingHalfBlock = boundingBox.checkBlocks(world, material -> material.getData() == Stairs.class || material.getData() == Step.class);
+        final boolean touchingClimbable = boundingBox.checkBlocks(world, material ->  material == Material.LADDER || material == Material.LAVA);
+
+        this.touchingAir.set(touchingAir);
+        this.touchingLiquid.set(touchingLiquid);
+        this.touchingHalfBlock.set(touchingHalfBlock);
+        this.touchingClimbable.set(touchingClimbable);
     }
 }
