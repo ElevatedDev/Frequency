9ca7e76ff379a550305d0fee20ec08d1c605ceef
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-07-24 00:23:03 +0300

    AutoClickerH

---
+++ src/main/java/xyz/elevated/frequency/check/impl/autoclicker/AutoClickerH.java
@@ -0,0 +1,45 @@
+package xyz.elevated.frequency.check.impl.autoclicker;
+
+import com.google.common.collect.Lists;
+import xyz.elevated.frequency.check.CheckData;
+import xyz.elevated.frequency.check.type.PacketCheck;
+import xyz.elevated.frequency.data.PlayerData;
+import xyz.elevated.frequency.util.MathUtil;
+import xyz.elevated.frequency.wrapper.impl.client.WrappedPlayInArmAnimation;
+import xyz.elevated.frequency.wrapper.impl.client.WrappedPlayInFlying;
+
+import java.util.Deque;
+
+@CheckData(name = "AutoClicker (H)")
+public final class AutoClickerH extends PacketCheck {
+    private int movements = 0;
+    private final Deque<Integer> samples = Lists.newLinkedList();
+
+    public AutoClickerH(final PlayerData playerData) {
+        super(playerData);
+    }
+
+    @Override
+    public void process(final Object object) {
+        if (object instanceof WrappedPlayInArmAnimation) {
+            final boolean valid = movements < 4 && !playerData.getActionManager().getDigging().get();
+
+            if (valid) samples.add(movements);
+
+            if (samples.size() == 20) {
+                final double cps = playerData.getCps().get();
+
+                final double average = samples.stream().mapToDouble(d -> d).average().orElse(0.0);
+                final double deviation = MathUtil.getStandardDeviation(samples);
+
+                final double duplicates = samples.size() - samples.stream().distinct().count();
+
+                if (cps > 7.d && average < 4 && deviation < 1.25 && duplicates > 10) fail();
+            }
+
+            movements = 0;
+        } else if (object instanceof WrappedPlayInFlying) {
+            movements++;
+        }
+    }
+}
