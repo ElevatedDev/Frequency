ab633485993db99b52961dcc716872cfa8c0b9da
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-10-12 21:53:56 +0300

    Straight push onto master for speed (urgent)

--- src/main/java/xyz/elevated/frequency/check/impl/speed/Speed.java
+++ src/main/java/xyz/elevated/frequency/check/impl/speed/Speed.java
@@ -3,9 +3,7 @@ package xyz.elevated.frequency.check.impl.speed;
 import net.minecraft.server.v1_8_R3.BlockPosition;
 import net.minecraft.server.v1_8_R3.EntityPlayer;
 import net.minecraft.server.v1_8_R3.MathHelper;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffectType;
 import xyz.elevated.frequency.check.CheckData;
@@ -15,6 +13,7 @@ import xyz.elevated.frequency.exempt.type.ExemptType;
 import xyz.elevated.frequency.update.PositionUpdate;
 import xyz.elevated.frequency.util.MathUtil;
 import xyz.elevated.frequency.util.NmsUtil;
+
 
 @CheckData(name = "Speed")
 public final class Speed extends PositionCheck {
@@ -27,8 +26,8 @@ public final class Speed extends PositionCheck {
     }
 
     /*
-    * Most values are found in the EntityLivingBase class on the client-side.
-    * They can even be found inside the EntityLiving nms class.
+     * Most values are found in the EntityLivingBase class on the client-side.
+     * They can even be found inside the EntityLiving nms class.
      */
 
     @Override
@@ -55,13 +54,13 @@ public final class Speed extends PositionCheck {
         final boolean exempt = this.isExempt(ExemptType.TPS, ExemptType.TELEPORTING);
 
         /*
-        * How minecraft calculates speed increase. We cast as a float because this is what the client does.
-        * MCP just prints the casted float as a double. 0.2 is the effect modifier.
+         * How minecraft calculates speed increase. We cast as a float because this is what the client does.
+         * MCP just prints the casted float as a double. 0.2 is the effect modifier.
          */
         attributeSpeed += MathUtil.getPotionLevel(player, PotionEffectType.SPEED) * (float)0.2 * attributeSpeed;
 
         //How minecraft calculates slowness. 0.15 is the effect modifier.
-        attributeSpeed += MathUtil.getPotionLevel(player, PotionEffectType.SLOW) * (float)-.15 * attributeSpeed;
+        attributeSpeed += MathUtil.getPotionLevel(player, PotionEffectType.SPEED) * (float)-.15 * attributeSpeed;
 
         if (onGround) {
             blockSlipperiness *= 0.91f;
@@ -72,16 +71,16 @@ public final class Speed extends PositionCheck {
             //Only do this when the player is sprinting. You dont move forward without sprinting my guy.
             if (deltaY > 0.4199 && playerData.getSprinting().get()) {
                 /*
-                * It's not necessary to do any angle work since it'll always be a factor of 0.2.
-                * Angle work is only necessary if we are checking motionX motionZ on its own, not together.
-                */
+                 * It's not necessary to do any angle work since it'll always be a factor of 0.2.
+                 * Angle work is only necessary if we are checking motionX motionZ on its own, not together.
+                 */
                 attributeSpeed += 0.2;
             }
         } else {
             /*
-            * We use the Player object as this will effectively be the previous tick.
-            * 0.026 is the value whe the player sprints, while 0.02 is when walking.
-            */
+             * We use the Player object as this will effectively be the previous tick.
+             * 0.026 is the value whe the player sprints, while 0.02 is when walking.
+             */
             attributeSpeed = playerData.getSprinting().get() ? 0.026 : 0.02;
 
             //This is basically the air resistance of the player.
