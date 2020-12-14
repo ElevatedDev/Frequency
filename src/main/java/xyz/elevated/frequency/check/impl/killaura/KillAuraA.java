4c5bfff1fea4f9009f959bf98d84c6a1456e8ad4
ElevatedDev <leo.kanarelis@digitaltreemedia.co.uk>
2020-12-14 16:06:02 +0200

    documentation

--- src/main/java/xyz/elevated/frequency/check/impl/killaura/KillAuraA.java
+++ src/main/java/xyz/elevated/frequency/check/impl/killaura/KillAuraA.java
@@ -1,6 +1,6 @@
 package xyz.elevated.frequency.check.impl.killaura;
 
-import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity;
+import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity.EnumEntityUseAction;
 import xyz.elevated.frequency.check.CheckData;
 import xyz.elevated.frequency.check.type.PostCheck;
 import xyz.elevated.frequency.data.PlayerData;
@@ -21,20 +21,38 @@ public final class KillAuraA extends PostCheck {
     @Override
     public void process(final Object object) {
         if (object instanceof WrappedPlayInFlying) {
+            final boolean empty = !sent;
+
             final long now = System.currentTimeMillis();
             final long delay = now - lastPacket;
 
-            if (sent) {
+            /*
+            * Essentially what we're doing here is trying to ensure that the packet got sent in the right
+            * order in comparison to the flying packet which is always sent in the end of the tick. If the
+            * PacketPlayInUseEntity packet is sent after the tick, the player's order is wrong. This is normally
+            * impossible since TCP always ensures the packets come in the right order.
+             */
+            post: {
+                if (empty) break post;
+
+                /*
+                * We're checking the distance between the flying packet and the last attack packet
+                * that was a valid entry. If the difference in time is outside of the range we desire,
+                * we're flagging the player. If not, we're decreasing the buffer.
+                 */
                 if (delay > 40L && delay < 100L) {
                     buffer += 0.25;
 
-                    if (buffer > 0.5) {
-                        fail();
-                    }
+                    if (buffer > 0.5) fail();
                 } else {
                     buffer = Math.max(buffer - 0.025, 0);
                 }
 
+                /*
+                * We need to make sure we reset the sent variable as it signifies that a valid
+                * attack entry was issued from the player. If we don't reset this the check will run every
+                * flying packet which we don't want since the player is not always attacking or sending a validated packet.
+                 */
                 sent = false;
             }
 
@@ -42,18 +60,33 @@ public final class KillAuraA extends PostCheck {
         } else if (object instanceof WrappedPlayInUseEntity) {
             final WrappedPlayInUseEntity wrapper = (WrappedPlayInUseEntity) object;
 
-            if (wrapper.getAction() != PacketPlayInUseEntity.EnumEntityUseAction.ATTACK) {
-                return;
-            }
+            /*
+            * We're validating the distance between the flying packet and the attack to make sure the
+            * player was really close to the last tick issued upon his attack. If he wasn't we can decrease
+            * the buffer, but if it was we can process the packet and it's timestamp to be issued in the tick.
+             */
+            validate: {
+                if (wrapper.getAction() != EnumEntityUseAction.ATTACK) break validate;
 
-            final long now = System.currentTimeMillis();
-            final long delay = now - lastFlying;
+                /*
+                * We're validating the packet through checking how close by the packet was to the
+                * end of the tick. We're doing this by checking the current time versus the last
+                * tick time. This is not optimal but it's fine for general use.
+                 */
+                final long now = System.currentTimeMillis();
+                final long delay = now - lastFlying;
 
-            if (delay < 10L) {
-                lastPacket = now;
-                sent = true;
-            } else {
-                buffer = Math.max(buffer - 0.025, 0.0);
+                /*
+                * Technically, an invalid timestamp would be any that is lower than 50ms, but
+                * when dealing with timestamps, which is not optimal, we have a larger margin of error,
+                * thus, we're running it through 10ms to make sure the check is functional.
+                 */
+                if (delay < 10L) {
+                    lastPacket = now;
+                    sent = true;
+                } else {
+                    buffer = Math.max(buffer - 0.025, 0.0);
+                }
             }
         }
     }
--- src/main/java/xyz/elevated/frequency/data/impl/ConnectionManager.java
+++ src/main/java/xyz/elevated/frequency/data/impl/ConnectionManager.java
@@ -13,26 +13,56 @@ public final class ConnectionManager {
     public void onTransaction(final short actionNumber, final long now) {
         final Optional<Long> entry = this.getTransactionTime(actionNumber);
 
+        /*
+        * Essentially, the ping of the player is the delay between the server and the client.
+        * We can easily get it by comparing the time it took for the client to respond to the transaction.
+        * And since the receivedTime - sentTime => delay, we can get it through the entry.
+         */
         entry.ifPresent(time -> playerData.getTransactionPing().set(now - time));
     }
 
     public void onKeepAlive(final int identification, final long now) {
         final Optional<Long> entry = this.getKeepAliveTime(identification);
 
+        /*
+        * Identically to transactions, the ping of the player is the delay between the server and the client.
+        * We can easily get it like transaction, basically by checking the difference of the time sent and the
+        * time received. So, receivedTime  sentTime => delay, meaning we can get it through the entry.
+         */
         entry.ifPresent(time -> playerData.getKeepAlivePing().set(now - time));
     }
 
+    /**
+     * @param actionNumber - The action-number of the transaction
+     * @return An Optional<Long> which contains the sent time.
+     */
     public Optional<Long> getTransactionTime(final short actionNumber) {
         final Map<Short, Long> entries = playerData.getTransactionUpdates();
 
+        /*
+        * If the variable is found within the map, we can easily parse it through an
+        * optional. If it's not found, then we return an empty optional. We could use the
+        * direct output from entries.get() method and wrap it with an optional but that's simply unsafe
+        * calling in development, thus, we're doing it safely.
+         */
         if (entries.containsKey(actionNumber)) return Optional.of(entries.get(actionNumber));
 
         return Optional.empty();
     }
 
+    /**
+     * @param identification - The server-tick of the keepalive
+     * @return An Optional<Long> which contains the sent time.
+     */
     public Optional<Long> getKeepAliveTime(final int identification) {
         final Map<Integer, Long> entries = playerData.getKeepAliveUpdates();
 
+        /*
+         * Likewise to transactions, we're trying to find the variable within the map and
+         * simply return an Optional.of() if found. If it's not found, we're returning an empt
+         * optional. This is generally a safer practice when it comes to programming and we follow it
+         * like we should.
+         */
         if (entries.containsKey(identification)) return Optional.of(entries.get(identification));
 
         return Optional.empty();
