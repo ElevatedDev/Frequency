84a2f368470aeea905f608d045b9a9bcddfd5fd9
frep <nickfrappier1217@gmail.com>
2021-01-06 12:50:00 -0500

    fixed backwards prediction

--- src/main/java/xyz/elevated/frequency/check/impl/fly/FlyB.java
+++ src/main/java/xyz/elevated/frequency/check/impl/fly/FlyB.java
@@ -23,7 +23,7 @@ public final class FlyB extends PositionCheck {
         final Location to = positionUpdate.getTo();
 
         final double deltaY = to.getY() - from.getY();
-        final double estimation = (lastDeltaY * 0.9800000190734863) - 0.08;
+        final double estimation = (lastDeltaY - 0.08) * 0.9800000190734863;
 
         final boolean resetting = Math.abs(deltaY) + 0.0980000019 < 0.05;
         final boolean exempt = this.isExempt(ExemptType.TELEPORTING, ExemptType.VELOCITY);
@@ -35,7 +35,7 @@ public final class FlyB extends PositionCheck {
         if (touchingAir) {
             ++ticks;
 
-            if (ticks > 5 && Math.abs(estimation - deltaY) > 0.002) {
+            if (ticks > 5 && Math.abs(estimation - deltaY) > 0.01) {
                 buffer += 1.5;
 
                 if (buffer > 5) fail();
